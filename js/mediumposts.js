9fa8d9df86a03bacb2ddca71269702f2c9e30de9
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2021-09-15 14:39:08 +0800

    Revert back change in RSS service provider

--- js/mediumposts.js
+++ js/mediumposts.js
@@ -7,7 +7,7 @@ function shortenText(text, startingPoint, maxLength) {
 }
 
 // featured post
-fetch('https://feed2json.org/convert?url=https://medium.com/feed/ownlyio/tagged/updates')
+fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/ownlyio/tagged/updates')
 .then(function (response) {
     return response.json();
 })
@@ -33,7 +33,7 @@ fetch('https://feed2json.org/convert?url=https://medium.com/feed/ownlyio/tagged/
 });
 
 // other posts
-fetch('https://feed2json.org/convert?url=https://medium.com/feed/ownlyio')
+fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/ownlyio')
 .then(function (response) {
     return response.json();
 })
