82746c0d99a2bc26fc15cd9fea99cab4475a4300
Bernard Historillo <bernard.historillo@bicol-u.edu.ph>
2022-07-20 10:11:44 +0800

    Add Boy Dibil collection countdown

--- js/script.js
+++ js/script.js
@@ -62,8 +62,8 @@ let pad_zeroes = (number) => {
 
 let start_countdown = () => {
     $.ajax({
-        url: "https://ownly.market/api/get-remaining-time/2022-06-27%2012:00:00",
-        // url: "http://ownly-api.test/api/get-remaining-time/2021-09-30%2009:00:00",
+        url: "https://ownly.market/api/get-remaining-time/2022-07-28%2012:00:00",
+        // url: "http://ownly-api.test/api/get-remaining-time/2022-07-28%2012:00:00",
         method: "GET"
     }).done(function(remaining_time) {
         let countDownDate = new Date().getTime() + (remaining_time * 1000);
@@ -124,7 +124,7 @@ $(window).on("load", function() {
 
 $(document).ready(function() {
     initiate_loading_page();
-    // start_countdown();
+    start_countdown();
 });
 
 $(document).on("mouseover", ".artist-card", function() {
